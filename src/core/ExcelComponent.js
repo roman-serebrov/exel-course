b5594c8a18884405b1bff404b4456d86057b2133
Роман <roman@MacBook-Air-Roman.local>
2020-08-23 17:34:58 +0300

    Finish base fraimworck fuctional

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,18 @@
+import {DomListener} from "@core/DomListener";
+
+export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners);
+        this.name = options.name
+    }
+    // возврощает шаблон компанента
+    toHtml() {
+       return ''
+    }
+    init() {
+        this.initDOMListeners()
+    }
+    destroy() {
+        this.removeDOMListeners()
+    }
+}
